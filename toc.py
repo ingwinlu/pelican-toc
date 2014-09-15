1274778ba52c018285fa77dd1a2d8d8014f0b761
winlu <derwinlu+git@gmail.com>
2014-09-15 13:31:55 +0200

    add basic extr

--- toc.py
+++ toc.py
@@ -16,38 +16,45 @@ class HtmlTreeNode:
         self.header = header
     
     def add(self, new_header):
-        if (self.header==None):
-            return HtmlTreeNode(None, new_header)
-        
-        if (new_header.name > self.header.name):
-            #can insert as child
-            new_node = HtmlTreeNode(self,new_header)
-            self.childs = self.childs ++ [new_node]
+        if(self.header==None or self.header.name < new_header.name):
+            #can add as child
+            new_node = HtmlTreeNode(self, new_header)
+            self.childs = self.childs + [new_node]
             return new_node
-        elif (new_header.name <= self.header.name):
-            if(self.parent!=None):
-                return self.parent.add(new_header)
+        elif(self.header.name == new_header.name):
+            #new sibling
+            new_node = HtmlTreeNode(self.parent, new_header)
+            self.parent.childs = self.parent.childs + [new_node]
+            return new_node
+        elif(self.header.name > new_header.name):
+            #new parent
+            if(self.parent == None):
+                new_node = HtmlTreeNode(None, new_header)
+                self.parent = new_node
+                new_node.childs = [self]
+                return new_node
             else:
-                #cant go up higher, make new parent
-                new_parent = HtmlTreeNode(None,new_header)
-                new_parent.add(self)
-                return new_parent
+                return self.parent.add(new_header)
+    
+    def __repr__(self):
+        ret = repr(self.header) + " Childs:" + repr(self.childs)
+        return ret
 
-
-def generate_toc(instance):
-    if instance._content is not None:
-        toc = {}
-        node = HtmlTreeNode(None, None)
-        
-        content = instance._content
-        soup = BeautifulSoup(content)
-        for header in soup.findAll(re.compile("^h\d")):
-            node = node.add(header)
-            #add to toclist
-            #set unique id if needed
-        print(repr(node))
-        input()
-        instance.toc = toc
+def generate_toc(content):
+    if isinstance(content, contents.Static):
+        return
+    
+    toc = []
+    #tree = node = HtmlTreeNode(None, None)
+    
+    soup = BeautifulSoup(content._content)
+    for header in soup.findAll(re.compile("^h\d")):
+        toc = toc + [header.prettify(formatter="html")]
+        #node = node.add(header)
+        #add to toclist
+        #set unique id if needed
+    #content._content = soup.decode()
+    content.toc = toc
 
 
 def register():
